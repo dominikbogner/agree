c575403867fa0b379b9da8ba1d56d4949153f6f5
dominikbogner <41120628+dominikbogner@users.noreply.github.com>
2026-04-13 12:24:42 +0200

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,3 +1,13 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+  typescript: {
+    // Schaltet die speicherintensive TS-Prüfung beim Build aus
+    ignoreBuildErrors: true,
+  },
+  eslint: {
+    // Schaltet den speicherintensiven Linter beim Build aus
+    ignoreDuringBuilds: true,
+  },
+}
+
 export default nextConfig
