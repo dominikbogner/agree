3abccbd54b79c87901e259f796b910800d3c0eba
dominikbogner <41120628+dominikbogner@users.noreply.github.com>
2026-04-13 14:06:07 +0200

    Update middleware.ts

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,4 +1,5 @@
 export { default } from "next-auth/middleware"
+
 export const config = {
-  matcher: ["/dashboard/:path*", "/polls/new/:path*"],
+  matcher: ["/dashboard/:path*", "/polls/new/:path*", "/polls/:id/settings/:path*"],
 }
